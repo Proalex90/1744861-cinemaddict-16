e55cb8ed19456d7ee31dd07a1368571eafd06fc0
Proalex90 <79789689+Proalex90@users.noreply.github.com>
2021-11-29 18:56:38 +0300

    Добавляет компонентр профиля

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { creatFilmsTemlate } from './view/films.js';
 import { creatButtonShowMore } from './view/show-more.js';
 import { creatStatsTemplate } from './view/stats.js';
 import { creatPopupFilmDetails } from './view/popup.js';
+import { createProfileHeaderTemplate } from './view/profile.js';
 
+const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooter = document.querySelector('.footer');
 
@@ -19,7 +21,7 @@ const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-
+renderTemplate(siteHeaderElement, createProfileHeaderTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createMenuTemlate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, creatFilmsTemlate(), RenderPosition.BEFOREEND);
 
@@ -31,5 +33,13 @@ for (let i = 0; i < 5; i++) {
 }
 
 renderTemplate(siteFilmsContainer, creatButtonShowMore(), RenderPosition.AFTEREND);
+
 renderTemplate(siteMainElement, creatStatsTemplate(), RenderPosition.BEFOREEND);
+const statisticElement = document.querySelector('.statistic');
+statisticElement.classList.add('visually-hidden'); //временный функционал
+
+
 renderTemplate(siteFooter, creatPopupFilmDetails(), RenderPosition.AFTEREND);
+const filmDetails = document.querySelector('.film-details');
+filmDetails.classList.add('visually-hidden'); //временный функционал
+
