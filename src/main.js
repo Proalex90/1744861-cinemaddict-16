0f2feae83cf9d61a9bf411e99dbb10d1da41df3a
Proalex90 <79789689+Proalex90@users.noreply.github.com>
2021-12-13 20:06:34 +0300

    Добавляет блок EXTRA и Profile

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { renderTemplate, renderElement, RenderPosition } from './render.js';
 import SiteMenuView from './view/menu.js';
+import SiteProfileView from './view/profile.js';
 import SiteMenuSortView from './view/menu-sort.js';
 import SiteFilmsView from './view/films.js';
+import ButtonMoreView from './view/show-more.js';
 import { creatCardFilm } from './view/card-film.js';
-import { creatButtonShowMore } from './view/show-more.js';
+import { createFilmsExtra } from './view/films-extra.js';
 import { creatStatsTemplate } from './view/stats.js';
 import { creatPopupFilmDetails } from './view/popup.js';
-import { createProfileHeaderTemplate } from './view/profile.js';
 import { generateFilmCard } from './mocks/film.js';
 import { creatComment } from './view/comment.js';
 const siteHeaderElement = document.querySelector('.header');
@@ -14,7 +15,7 @@ const siteMainElement = document.querySelector('.main');
 const siteFooter = document.querySelector('.footer');
 
 
-renderTemplate(siteHeaderElement, createProfileHeaderTemplate(), RenderPosition.BEFOREEND); //Элемент профайла
+renderElement(siteHeaderElement, new SiteProfileView().element, RenderPosition.BEFOREEND); //Элемент профайла
 
 renderElement(siteMainElement, new SiteMenuView().element, RenderPosition.BEFOREEND); //Основное меню
 renderElement(siteMainElement, new SiteMenuSortView().element, RenderPosition.BEFOREEND);//Сортировка основного меню
@@ -32,10 +33,9 @@ for (let i = 0; i < Math.min(films.length, FILMS_COUNT_PER_STEP); i++) {
 
 if (films.length > FILMS_COUNT_PER_STEP) {
   let renderedFilmsCount = FILMS_COUNT_PER_STEP;
-
-  renderTemplate(siteFilmsContainer, creatButtonShowMore(), RenderPosition.AFTEREND);
-  const buttonShowMore = document.querySelector('.films-list__show-more');
-  buttonShowMore.addEventListener(('click'), (evt) => {
+  const buttonShowMoreComponent = new ButtonMoreView();
+  renderElement(siteFilmsContainer, buttonShowMoreComponent.element, RenderPosition.AFTEREND);
+  buttonShowMoreComponent.element.addEventListener(('click'), (evt) => {
     evt.preventDefault();
     films
       .slice(renderedFilmsCount, renderedFilmsCount + FILMS_COUNT_PER_STEP)
@@ -43,11 +43,23 @@ if (films.length > FILMS_COUNT_PER_STEP) {
     renderedFilmsCount += FILMS_COUNT_PER_STEP;
 
     if (renderedFilmsCount >= films.length) {
-      buttonShowMore.remove();
+      buttonShowMoreComponent.element.remove();
     }
   });
 }
 
+renderTemplate(filmsListComponent.element, createFilmsExtra(), RenderPosition.BEFOREEND);
+const FILMS_EXTRA_COUNT = 2;
+const siteFilmsExtra = document.querySelectorAll('.films-list--extra');
+const siteFilmsExtraContainerTopRated = siteFilmsExtra[0].querySelector('.films-list__container');
+const siteFilmsExtraContainerMostCommented = siteFilmsExtra[1].querySelector('.films-list__container');
+
+for (let i = 0; i < FILMS_EXTRA_COUNT; i++) {
+  renderTemplate(siteFilmsExtraContainerTopRated, creatCardFilm(films[i]), RenderPosition.BEFOREEND);
+}
+for (let i = 0; i < FILMS_EXTRA_COUNT; i++) {
+  renderTemplate(siteFilmsExtraContainerMostCommented, creatCardFilm(films[i]), RenderPosition.BEFOREEND);
+}
 
 renderTemplate(siteFooter, creatPopupFilmDetails(films[0]), RenderPosition.AFTEREND);
 
--- src/view/show-more.js
+++ src/view/show-more.js
@@ -1,2 +1,24 @@
-export const creatButtonShowMore = () => `<button class="films-list__show-more">Show more
+import { createElement } from '../render.js';
+
+
+const createButtonShowMore = () => `<button class="films-list__show-more">Show more
 </button>`;
+
+export default class ButtonMoreView {
+  #element = null;
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+    return this.#element;
+  }
+
+  get template() {
+    return createButtonShowMore();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
